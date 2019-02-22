ef93de19e63747ba4d8173ef23307582ead4305b
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-22 13:28:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,9 @@ const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
 
+const myid = ['321476121956974595'];
+const prefix = ['-'];
+
 client.on('ready', () => {
    console.log(`----------------`);
    console.log(`Credit Farmm - Script By : Mina `);
@@ -15,11 +18,7 @@ client.on('ready', () => {
    console.log(`----------------`);
 });
 
-client2.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`2 Account Online ✩' `);
-   console.log(`----------------`);
-});
+
 
 
 
@@ -74,56 +73,6 @@ if (!rank) return message.reply(' ')
   }
 });
 
-client2.on('message', message => {
-    if(message.content === prefix+'راتب'){
-        message.channel.send('#daily')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === prefix+'مبلغ'){
-        message.channel.send('#credits')
-    }
-});
-
-client2.on('message', message => {
-    if(message.content === prefix+'ريب'){
-        message.channel.send('#rep <@321476121956974595>')
-    }
-});
-
-client2.on('message', message => {
-if (message.content === prefix+'spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-client2.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-
-if (command == "تحدث") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
 
 client.on("ready", () => {
 let channel =     client.channels.get("510168817494654988")
