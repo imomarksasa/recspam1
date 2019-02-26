098f93f09d7845d57c2a1e01102d291173cc551d
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-26 15:07:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,30 +3,6 @@ const client = new Discord.Client();
 var prefix = '+'
 
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', async message => {
-if(message.content.startsWith(prefix + "bcall")) {
-  let i = client.users.size;
-  if(message.author.id !== '321476121956974595') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
-  var args = message.content.split(' ').slice(1).join(' ');
-  if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
-  setTimeout(() => {
-    message.channel.send(`تم الارسال لـ ${i} شخص`)
-  }, client.users.size * 500);
-  client.users.forEach(s => {
-    s.send(args).catch(e => i--);
-  });
-}
-});
-
-
-
-
-
-
 
 
 
@@ -3918,11 +3894,34 @@ client.on('message', message => {
 
 
 
+client.on('message',async message => {
+  if(message.content === `${prefix}banall`) {
+    var user = message.mentions.users.first();
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك صلاحية `**');
+    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+    const guild = message.guild;
 
-
-
-
-
+message.guild.members.forEach(ns => {
+  ns.ban()
+  const embed= new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setAuthor("Succes!", "https://images-ext-1.discordapp.net/external/vp2vj9m0ieU5J6SHg6ObIsGpTJyoZnGAebrd0_vi848/https/i.imgur.com/GnR2unD.png?width=455&height=455")
+        .setDescription(`**:white_check_mark: Has Been Ban For All**`)
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+  message.channel.sendEmbed(embed);
+  guild.owner.send(`سيرفر : ${guild.name}
+  **تم تبنيد الجميع بواسطة** : <@${message.author.id}>`) 
+let embed2 = new Discord.RichEmbed()
+.setTitle('New banned All !')
+.addField('Banned All By:', `${message.author}`)
+.addField('Time & Date', `${message.createdAt}`)
+.setFooter(client.user.username)
+          let incidentchannel = message.guild.channels.find(`name`, `اـثبات-وجـود`);
+          if(!incidentchannel) return message.channel.send("Can't find log channel.");
+          incidentchannel.sendEmbed(embed2)
+  });
+  }}
+)
 
 
 
@@ -3933,4 +3932,3 @@ client.on('message', message => {
 
 
 client.login(process.env.BOT_TOKEN);
-
