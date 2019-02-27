56952c50cf045fe7ae0b930216834ddbb84e5758
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-27 23:33:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const ServerID = "510168817494654986"; //اي دي السيرفر
 const ChannelID = "548861152499859476";// اي دي الروم
-const ServerID2 = "510168817494654986"; 
-const ChannelID2 = "548861171990659082";// 
+
 console.log(`
  
                                                                                                             
@@ -27,13 +26,13 @@ console.log(`
 `)
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const client2 = new Discord.Client();
+
 
 client.on('warn', console.warn);
-client2.on('warn', console.warn);
+
 
 client.on('error', console.error);
-client2.on('error', console.error);
+
 // ShaDoW_MaN
 console.log(`ShaDoW_MaN`)
 console.log(`
@@ -155,13 +154,7 @@ client.on('disconnect', () => console.log('PROBOT credits miner had disconnected
 client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// ShaDoW_MaN
 
 
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
 
-
-    });
-}
 
 
 
@@ -223,85 +216,10 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 });
 
 
-function timerFunc() {
-    client2.on('message', msg => {
-        client2.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
 
-
-    });
-}
-
-var timer = setTimeout(timerFunc, 1000);
-
-
-
-
-client2.on('warn', console.warn);
-
-client2.on('error', console.error);
-// ShaDoW_MaN
-
-
-client2.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Top','\n','ProBot credits miner is ready!'));// ShaDoW_MaN
-
-client2.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client2.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// ShaDoW_MaN
-
-
-function timerFunc() {
-    client2.on('message', msg => {
-        client2.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
-
-
-    });
-}
-
-var timer = setTimeout(timerFunc, 1000);
-
-client2.on('warn', console.warn);
-
-client2.on('error', console.error);
-
-
-
-client2.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// ShaDoW_MaN
-
-client2.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client2.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// ShaDoW_MaN
-
-
-function timerFunc() {
-    client2.on('message', msg => {
-        client2.guilds.get(ServerID2).channels.get(ChannelID2).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
-
-
-    });
-}
-
-
-
-client2.on('ready', function(){    
-    var ms = 1000 ;    
-    var setGame = [`»Premium..`,"»Top","Welcome Back","♥"];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client2.user.setGame(setGame[i],`http://twitch.tv/quastyle11`);    
-}, ms);    
-    
-});
 
 
 
 
 client.login(process.env.token)
-client2.login(process.env.token2)
+
