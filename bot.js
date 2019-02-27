75ffc055af8bfce9ec92c9b9d4af68719603b835
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-27 22:59:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,8 +29,10 @@ const client = new Discord.Client();
 const client2 = new Discord.Client();
 
 client.on('warn', console.warn);
+client2.on('warn', console.warn);
 
 client.on('error', console.error);
+client2.on('error', console.error);
 // ShaDoW_MaN
 console.log(`ShaDoW_MaN`)
 console.log(`
@@ -220,7 +222,82 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 });
 
 
+function timerFunc() {
+    client2.on('message', msg => {
+        client2.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
 
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
+
+
+
+client2.on('warn', console.warn);
+
+client2.on('error', console.error);
+// ShaDoW_MaN
+
+
+client2.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Top','\n','ProBot credits miner is ready!'));// ShaDoW_MaN
+
+client2.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client2.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// ShaDoW_MaN
+
+
+function timerFunc() {
+    client2.on('message', msg => {
+        client2.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
+client2.on('warn', console.warn);
+
+client2.on('error', console.error);
+
+
+
+client2.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// ShaDoW_MaN
+
+client2.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client2.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// ShaDoW_MaN
+
+
+function timerFunc() {
+    client2.on('message', msg => {
+        client2.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // ShaDoW_MaN
+
+
+    });
+}
+
+
+
+client2.on('ready', function(){    
+    var ms = 1000 ;    
+    var setGame = [`»Premium..`,"»Top","Welcome Back","♥"];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://twitch.tv/quastyle11`);    
+}, ms);    
+    
+});
 
 
 
